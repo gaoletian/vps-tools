2fca05c7972295fe1b445773c6a8a36420184209
gaoletian <gaolet@chanjet.com>
2023-10-26 12:08:54 +0800

    first commit

---
+++ use-root-ssh.sh
@@ -0,0 +1,4 @@
+echo root:密码设置 |sudo chpasswd root
+sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
